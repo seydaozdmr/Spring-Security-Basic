8907680da37b24b46c3eee2f10197a99c883cbab
tableonthewall <seydaozdemir07@gmail.com>
2021-01-24 21:04:20 +0300

    24/01/2021 Threads-synchronized

--- src/main/java/com/security/demo/Threads/CountDown_4.java
+++ src/main/java/com/security/demo/Threads/CountDown_4.java
@@ -10,6 +10,9 @@ public class CountDown_4 implements ApplicationListener<ContextRefreshedEvent>{
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         CountDown countDown=new CountDown();
+        //Eğer ayrı ayrı nesneler oluşturursak bunları kullanan Threadler ayrı ayrı çalışır.
+        //CountDown countDown2=new CountDown();
+
         CountdownThread t1=new CountdownThread(countDown);
         t1.setName("Thread 1");
         CountdownThread t2=new CountdownThread(countDown);
@@ -26,6 +29,7 @@ public class CountDown_4 implements ApplicationListener<ContextRefreshedEvent>{
         //Aksi durumda burada instance variable kullanıyoruz. Bu durumda iki Thread'de aynı insantance paylaşacaklar.
         //Bu durumda heap 'de yer ayrılıyor ve aynı nesne örneğini kullandıkları için aynı instance değişkeni kullanıyorlar.
         private int i;
+        //Bu metodu çalıştıran Thread'lerin ayrı ayrı karışmadan çalışmasını (interference) istiyorsak synchronized kullanmamız gerekiyor.
         public void doCountDown(){
             String color;
             switch (Thread.currentThread().getName()){
@@ -38,10 +42,13 @@ public class CountDown_4 implements ApplicationListener<ContextRefreshedEvent>{
                 default:
                     color=ThreadColor.ANSI_GREEN;
             }
+            synchronized (this){
+                for(i=10;i>0;i--){
+                    System.out.println(color+ Thread.currentThread().getName()+" : i = "+i);
+                }
 
-            for(i=10;i>0;i--){
-                System.out.println(color+ Thread.currentThread().getName()+" : i = "+i);
             }
+
 
         }
     }
