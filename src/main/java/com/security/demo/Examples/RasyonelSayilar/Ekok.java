4d92f93264b030b1cd4db8a68c5854092c7dd120
tableonthewall <seydaozdemir07@gmail.com>
2021-02-23 12:47:08 +0300

    23/02/2021

---
+++ src/main/java/com/security/demo/Examples/RasyonelSayilar/Ekok.java
@@ -0,0 +1,41 @@
+package com.security.demo.Examples.RasyonelSayilar;
+
+public class Ekok {
+    public static void main(String[] args) {
+        System.out.println(ekok(3,4,5));
+        System.out.println("-----------------");
+        System.out.println(ekok(17,91));
+        System.out.println("-----------------");
+        System.out.println(ekok(6,4,5));
+    }
+
+
+    public static int ekok(Integer ... sayilar){
+        int numberOfArguments=sayilar.length;
+        int enBuyuk=0;
+        for(int i=0;i<numberOfArguments;i++){
+            if(sayilar[i]>enBuyuk){
+                enBuyuk=sayilar[i];
+            }
+        }
+        System.out.println("en büyük sayı: "+enBuyuk);
+        int ekok=1;
+            for(int j=0;j<numberOfArguments;j++){
+                int tamBolen=1;
+                int bolunenArguman=0;
+                for(int i=2;i<=enBuyuk;i++){
+                    if(sayilar[j]%i==0){
+                        bolunenArguman++;
+                        tamBolen=i;
+                        System.out.println(sayilar[j]+" tam bölen= "+tamBolen );
+                        sayilar[j]/=i;
+                        System.out.println("Bölüm sonrası değer "+sayilar[j]);
+                        ekok*=tamBolen;
+                        j=0;
+                    }
+                }
+                //TODO bölümleri bir listeye kaydetip ortak katların en küçüklerini almalıyım
+            }
+        return ekok;
+    }
+}
