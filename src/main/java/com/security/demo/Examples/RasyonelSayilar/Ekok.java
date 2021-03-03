ea6741f272cb77499f3cd0f13e008a9939398ff6
tableonthewall <seydaozdemir07@gmail.com>
2021-03-03 20:34:27 +0300

    Update Ekok.java

--- src/main/java/com/security/demo/Examples/RasyonelSayilar/Ekok.java
+++ src/main/java/com/security/demo/Examples/RasyonelSayilar/Ekok.java
@@ -1,5 +1,8 @@
 package com.security.demo.Examples.RasyonelSayilar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Ekok {
     public static void main(String[] args) {
         System.out.println(ekok(3,4,5));
@@ -13,6 +16,8 @@ public class Ekok {
     public static int ekok(Integer ... sayilar){
         int numberOfArguments=sayilar.length;
         int enBuyuk=0;
+        Map<Integer,Integer> bolunenSayilar=new HashMap<>();
+
         for(int i=0;i<numberOfArguments;i++){
             if(sayilar[i]>enBuyuk){
                 enBuyuk=sayilar[i];
@@ -25,11 +30,13 @@ public class Ekok {
                 int bolunenArguman=0;
                 for(int i=2;i<=enBuyuk;i++){
                     if(sayilar[j]%i==0){
+                        
                         bolunenArguman++;
                         tamBolen=i;
                         System.out.println(sayilar[j]+" tam bölen= "+tamBolen );
                         sayilar[j]/=i;
                         System.out.println("Bölüm sonrası değer "+sayilar[j]);
+
                         ekok*=tamBolen;
                         j=0;
                     }
