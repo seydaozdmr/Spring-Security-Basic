1feab6c6e46688cfdfaa0e56fc530f0fd07e3d2e
tableonthewall <seydaozdemir07@gmail.com>
2021-03-17 00:23:25 +0300

    17/03/2021 - some examples

--- src/main/java/com/security/demo/Examples/RasyonelSayilar/Ekok.java
+++ src/main/java/com/security/demo/Examples/RasyonelSayilar/Ekok.java
@@ -1,48 +1,69 @@
 package com.security.demo.Examples.RasyonelSayilar;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Ekok {
     public static void main(String[] args) {
-        System.out.println(ekok(3,4,5));
-        System.out.println("-----------------");
-        System.out.println(ekok(17,91));
-        System.out.println("-----------------");
-        System.out.println(ekok(6,4,5));
+//        System.out.println(ekok(3,4,5));
+//        System.out.println("-----------------");
+//        System.out.println(ekok(17,91));
+//        System.out.println("-----------------");
+//        System.out.println(ekok(6,4,5));
+        Map<Integer,Integer> ekoklistesi=ekok(4,5,8);
+        ekoklistesi.forEach((k,v)-> System.out.println("bölüm : "+k+ "kaç kez: "+v));
+        int ekok=1;
+        for(Map.Entry deger: ekoklistesi.entrySet()){
+            ekok*=(Integer)deger.getKey()*(Integer)deger.getValue();
+        }
+        System.out.println("ekok : "+ekok);
     }
 
 
-    public static int ekok(Integer ... sayilar){
+    public static Map<Integer,Integer> ekok(Integer ... sayilar){
         int numberOfArguments=sayilar.length;
         int enBuyuk=0;
         Map<Integer,Integer> bolunenSayilar=new HashMap<>();
-
+        Map<Integer,Integer> ekokListesi=new HashMap<>();
         for(int i=0;i<numberOfArguments;i++){
             if(sayilar[i]>enBuyuk){
                 enBuyuk=sayilar[i];
             }
         }
-        System.out.println("en büyük sayı: "+enBuyuk);
-        int ekok=1;
             for(int j=0;j<numberOfArguments;j++){
-                int tamBolen=1;
-                int bolunenArguman=0;
                 for(int i=2;i<=enBuyuk;i++){
                     if(sayilar[j]%i==0){
-                        
-                        bolunenArguman++;
-                        tamBolen=i;
-                        System.out.println(sayilar[j]+" tam bölen= "+tamBolen );
+                        System.out.println("sayi : "+sayilar[j]);
+                        if(!bolunenSayilar.containsKey(i)){
+                            bolunenSayilar.put(i,1);
+                        }else{
+                            bolunenSayilar.put(i,bolunenSayilar.get(i)+1);
+                        }
                         sayilar[j]/=i;
-                        System.out.println("Bölüm sonrası değer "+sayilar[j]);
-
-                        ekok*=tamBolen;
+                        System.out.println("sayi2 : "+sayilar[j]);
+                        i=2;
                         j=0;
                     }
                 }
-                //TODO bölümleri bir listeye kaydetip ortak katların en küçüklerini almalıyım
+                bolunenSayilar.forEach((k,v)-> System.out.println("bölen : "+k+ "bölüm : "+v));
+
+                for(Map.Entry bolumDeger: bolunenSayilar.entrySet()){
+                    if(!ekokListesi.containsKey(bolumDeger.getKey())){
+                        ekokListesi.put((Integer)bolumDeger.getKey(),(Integer)bolumDeger.getValue());
+                        System.out.println("bölüm : "+bolumDeger.getKey()+" kaç kez :"+bolumDeger.getValue());
+                    }else{
+                        if((Integer)bolumDeger.getValue()<=ekokListesi.get(bolumDeger.getKey())){
+                            ekokListesi.put((Integer) bolumDeger.getKey(),(Integer) bolumDeger.getValue());
+                            System.out.println("bölüm2 : "+bolumDeger.getKey()+" kaç kez2 :"+bolumDeger.getValue());
+                        }else{
+                            ekokListesi.put((Integer) bolumDeger.getKey(),(Integer) bolumDeger.getValue()+1);
+                        }
+                    }
+                }
             }
-        return ekok;
+
+        //bolunenSayilar.forEach((k,v)-> System.out.println("bölen: "+k+" kaç kez: "+v));
+        return ekokListesi;
     }
 }
