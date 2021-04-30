3e0cf3d83d8c88e0ebb07a70e1c0c006f33c9337
tableonthewall <seydaozdemir07@gmail.com>
2021-05-01 01:35:20 +0300

    01/05/2021 threads

--- src/main/java/com/security/demo/Examples/OracleOCP/BuiltinLambdaExpression/PredicateExample.java
+++ src/main/java/com/security/demo/Examples/OracleOCP/BuiltinLambdaExpression/PredicateExample.java
@@ -1,8 +1,12 @@
 package com.security.demo.Examples.OracleOCP.BuiltinLambdaExpression;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.DoubleToIntFunction;
+import java.util.stream.Collectors;
 
 public class PredicateExample {
     public static void main(String[] args) {
@@ -18,6 +22,21 @@ public class PredicateExample {
 
         Consumer<String> action=s-> System.out.println(s);
         strings.forEach(action);
+
+        DoubleToIntFunction function=v->(int) v;
+        int pi=function.applyAsInt(Math.PI);
+        System.out.println(pi);
+
+        List<String> s=new ArrayList<>();
+        s.add("one");
+        s.add("two");
+        s.add("three");
+        s.add("four");
+        s.add("five");
+        s.add("six");
+        Comparator<String> str=(s1,s2)->s1.compareTo(s2);
+        Collections.sort(s,str);
+        System.out.println(s);
 
 
 
