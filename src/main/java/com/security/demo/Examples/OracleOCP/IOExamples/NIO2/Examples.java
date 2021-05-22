0003875b5a313e9258458ef4471c10b529a81570
tableonthewall <seydaozdemir07@gmail.com>
2021-05-22 17:19:22 +0300

    22/05/2021 IO Examples

---
+++ src/main/java/com/security/demo/Examples/OracleOCP/IOExamples/NIO2/Examples.java
@@ -0,0 +1,38 @@
+package com.security.demo.Examples.OracleOCP.IOExamples.NIO2;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Examples {
+    public static void main(String[] args) {
+        Path path= Paths.get("C:\\test\\test.txt").getParent().normalize().toAbsolutePath();
+        System.out.println(path.getFileName()); //current file name
+        System.out.println(path);
+
+        Path path2=Paths.get("C:\\","\\test\\test.txt");
+        System.out.println(path2);
+        System.out.println(path2.getFileName());
+
+        for(int i=0;i<path2.getNameCount();i++){
+            System.out.println("Element "+i+" is: "+path2.getName(i));
+        }
+
+        System.out.println("subpath(0,1) : "+path2.subpath(0,1));
+        System.out.println("subpath(0,2) : "+path2.subpath(0,2));
+
+        Path relativePath=Paths.get("\\Home");
+        System.out.println(relativePath.getFileName());
+        System.out.println(relativePath.isAbsolute());
+        System.out.println(relativePath.getParent());
+        while((relativePath=relativePath.getParent())!=null){
+            System.out.println("parent : "+relativePath);
+        }
+
+        while((path=path.getParent())!=null){
+            System.out.println("parent : "+path);
+        }
+
+        Path absoluteExample=relativePath.toAbsolutePath();
+        System.out.println(absoluteExample);
+    }
+}
---
+++ src/main/java/com/security/demo/Examples/OracleOCP/Singleton/BillPughExample.java
@@ -0,0 +1,17 @@
+package com.security.demo.Examples.OracleOCP.Singleton;
+
+public class BillPughExample {
+    //private constructor
+    private BillPughExample(){
+
+    }
+
+    public static BillPughExample getInstance(){
+        return SingletonExample.billPughExample;
+    }
+
+    //yardımcı sınıf içinde static instance oluşturuyor.
+    private static class SingletonExample{
+        private static BillPughExample billPughExample=new BillPughExample();
+    }
+}
