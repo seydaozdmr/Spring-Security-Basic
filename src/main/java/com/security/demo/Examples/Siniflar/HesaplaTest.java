a82b604f7e3fc66c81747bcf9da9e784812d9a68
tableonthewall <seydaozdemir07@gmail.com>
2021-06-16 23:12:38 +0300

    16/06/2021 classes

--- src/main/java/com/security/demo/Examples/Siniflar/HesaplaTest.java
+++ src/main/java/com/security/demo/Examples/Siniflar/HesaplaTest.java
@@ -2,6 +2,12 @@ package com.security.demo.Examples.Siniflar;
 
 public class HesaplaTest {
     public static void main(String[] args) {
+        //Not : java.lang paketinin tamamı otomatik olarak import edilir.
+
+        double result = Math. pow(2,3);
+        System.out.println(result);
+
+
         Hesapla hesapla=new Hesapla();
 
         int a=19;
