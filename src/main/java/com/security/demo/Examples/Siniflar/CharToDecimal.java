1ce93830750ba72cc210a512f9bd660d328ff76a
tableonthewall <seydaozdemir07@gmail.com>
2021-06-22 00:49:55 +0300

    22/06/2021 mydate example

--- src/main/java/com/security/demo/Examples/Siniflar/CharToDecimal.java
+++
@@ -1,7 +0,0 @@
-package com.security.demo.Examples.Siniflar;
-
-public class CharToDecimal {
-    public static void main(String[] args) {
-//        char sayi=
-    }
-}
---
+++ src/main/java/com/security/demo/Examples/Siniflar/SomeDateExamples/Example.java
@@ -0,0 +1,7 @@
+package com.security.demo.Examples.Siniflar.SomeDateExamples;
+
+public class Example {
+    public static void main(String[] args) {
+        
+    }
+}
