2c0c59afe83ab76155c26075a125f70942a36298
tableonthewall <seydaozdemir07@gmail.com>
2021-06-27 22:42:59 +0300

    27/06/2021 abstract animals

---
+++ src/main/java/com/security/demo/Examples/Siniflar/AbstractAnimals/Animal.java
@@ -0,0 +1,12 @@
+package com.security.demo.Examples.Siniflar.AbstractAnimals;
+
+public abstract class Animal {
+    private String name;
+    private double weight;
+    private int age;
+
+    //abstract methods
+    public abstract void getDosage();
+    public abstract long getFeedSchedule();
+
+}
---
+++ src/main/java/com/security/demo/Examples/Siniflar/AbstractAnimals/Horses.java
@@ -0,0 +1,14 @@
+package com.security.demo.Examples.Siniflar.AbstractAnimals;
+
+public class Horses extends Animal{
+
+    @Override
+    public void getDosage() {
+
+    }
+
+    @Override
+    public long getFeedSchedule() {
+        return 0;
+    }
+}
