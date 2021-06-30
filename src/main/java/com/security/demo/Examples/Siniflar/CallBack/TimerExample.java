3bf182412b0419f608371e246d6d329190535e58
tableonthewall <seydaozdemir07@gmail.com>
2021-06-30 23:56:29 +0300

    30/06/2021 timer anonymous class example

--- src/main/java/com/security/demo/Examples/Siniflar/CallBack/TimerExample.java
+++ src/main/java/com/security/demo/Examples/Siniflar/CallBack/TimerExample.java
@@ -10,12 +10,24 @@ public class TimerExample {
 
     public static void main(String[] args) throws InterruptedException {
         Timer t1=new Timer(1_000,new TimePrinter());
-        t1.start();
+        //t1.start();
 
         //Thread.sleep(1000);
 
-        JOptionPane.showMessageDialog(null,"Quit Program");
-        System.exit(0);
+//        JOptionPane.showMessageDialog(null,"Quit Program");
+//        System.exit(0);
+
+
+        //anonymous class
+        Timer t2=new Timer(2_000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Merhaba dünya");
+                Toolkit.getDefaultToolkit().beep();
+            }
+        });
+        t2.start();
+        //Thread.sleep(10000);
 
     }
 }
