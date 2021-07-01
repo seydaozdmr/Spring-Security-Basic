b46d5140abe7a6d9c967cad43c23176092f11643
tableonthewall <seydaozdemir07@gmail.com>
2021-07-02 00:53:18 +0300

    02/07/2021 classes examples

--- src/main/java/com/security/demo/Examples/Siniflar/HesaplaTest.java
+++ src/main/java/com/security/demo/Examples/Siniflar/HesaplaTest.java
@@ -1,5 +1,8 @@
 package com.security.demo.Examples.Siniflar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HesaplaTest {
     public static void main(String[] args) {
         //Not : java.lang paketinin tamamı otomatik olarak import edilir.
@@ -14,5 +17,14 @@ public class HesaplaTest {
         int b=100;
 
         int sonuc = hesapla.add(19  ,20);
+
+        int x=-10;
+        int y=-5;
+        System.out.println(x>y);
+        List<List<Integer>> liste=new ArrayList<>();
+        for(int i=0;i<10;i++){
+            liste.add(new ArrayList<>());
+        }
+
     }
 }
